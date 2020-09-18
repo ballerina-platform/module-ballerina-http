574841d2a0902e03f27423ff05f9d93e8373fb6d
manuranga perera <manu@wso2.com>
2020-09-18 12:18:15 +0530

    Migrate NonBlockingCallback to BalEnv

--- http-native/src/main/java/org/ballerinalang/net/http/websocket/WebSocketUtil.java
+++ http-native/src/main/java/org/ballerinalang/net/http/websocket/WebSocketUtil.java
@@ -27,6 +27,7 @@ import io.netty.handler.codec.http.websocketx.WebSocketHandshakeException;
 import org.ballerinalang.jvm.api.BErrorCreator;
 import org.ballerinalang.jvm.api.BStringUtils;
 import org.ballerinalang.jvm.api.BValueCreator;
+import org.ballerinalang.jvm.api.BalFuture;
 import org.ballerinalang.jvm.api.values.BError;
 import org.ballerinalang.jvm.api.values.BMap;
 import org.ballerinalang.jvm.api.values.BObject;
@@ -35,7 +36,6 @@ import org.ballerinalang.jvm.scheduling.Strand;
 import org.ballerinalang.jvm.services.ErrorHandlerUtils;
 import org.ballerinalang.jvm.types.BPackage;
 import org.ballerinalang.jvm.types.BType;
-import org.ballerinalang.jvm.values.connector.NonBlockingCallback;
 import org.ballerinalang.net.http.HttpConstants;
 import org.ballerinalang.net.http.HttpErrorType;
 import org.ballerinalang.net.http.HttpUtil;
@@ -116,30 +116,29 @@ public class WebSocketUtil {
         webSocketClient.set(WebSocketConstants.LISTENER_IS_OPEN_FIELD, webSocketConnection.isOpen());
     }
 
-    public static void handleWebSocketCallback(NonBlockingCallback callback,
+    public static void handleWebSocketCallback(BalFuture balFuture,
                                                ChannelFuture webSocketChannelFuture, Logger log,
                                                WebSocketConnectionInfo connectionInfo) {
         webSocketChannelFuture.addListener(future -> {
             Throwable cause = future.cause();
             if (!future.isSuccess() && cause != null) {
                 log.error(ERROR_MESSAGE, cause);
-                setCallbackFunctionBehaviour(connectionInfo, callback, cause);
+                setCallbackFunctionBehaviour(connectionInfo, balFuture, cause);
             } else {
                 // This is needed because since the same strand is used in all actions if an action is called before
                 // this one it will cause this action to return the return value of the previous action.
-                callback.setReturnValues(null);
-                callback.notifySuccess();
+                balFuture.complete(null);
             }
         });
     }
 
     public static void setCallbackFunctionBehaviour(WebSocketConnectionInfo connectionInfo,
-                                                    NonBlockingCallback callback, Throwable error) {
+                                                    BalFuture balFuture, Throwable error) {
         if (hasSupportForResiliency(connectionInfo)) {
             ErrorHandlerUtils.printError(error);
-            callback.notifySuccess();
+            balFuture.complete(null);
         } else {
-            callback.notifyFailure(WebSocketUtil.createErrorByType(error));
+            balFuture.complete(WebSocketUtil.createErrorByType(error));
         }
     }
 
@@ -589,8 +588,8 @@ public class WebSocketUtil {
         return exception;
     }
 
-    public static void setNotifyFailure(String msg, NonBlockingCallback callback) {
-        callback.notifyFailure(getWebSocketException(msg, null,
+    public static void setNotifyFailure(String msg, BalFuture balFuture) {
+        balFuture.complete(getWebSocketException(msg, null,
                 WebSocketConstants.ErrorCode.WsInvalidHandshakeError.errorCode(), null));
     }
 
