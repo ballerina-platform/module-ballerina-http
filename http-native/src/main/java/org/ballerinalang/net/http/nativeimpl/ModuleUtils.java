166a629c50d7738afa5ca219bac03598d50d206b
Chanaka Lakmal <ldclakmal@gmail.com>
2021-01-19 23:50:51 +0530

    Replace hard coded module version

---
+++ http-native/src/main/java/org/ballerinalang/net/http/nativeimpl/ModuleUtils.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.net.http.nativeimpl;
+
+import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.Module;
+import io.ballerina.runtime.api.utils.StringUtils;
+import io.ballerina.runtime.api.values.BString;
+
+/**
+ * This class will hold module related utility functions.
+ *
+ * @since 2.0.0
+ */
+public class ModuleUtils {
+
+    private static Module httpModule;
+
+    private ModuleUtils() {}
+
+    public static void setModule(Environment env) {
+        httpModule = env.getCurrentModule();
+    }
+
+    public static BString getModuleIdentifier() {
+        return StringUtils.fromString(httpModule.toString());
+    }
+}
