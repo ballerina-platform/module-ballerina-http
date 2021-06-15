be41ece8f47a4093a17197c0fee0bf0530cb9b8a
TharmiganK <ktharmi176@gmail.com>
2021-06-15 20:51:18 +0530

    Add Junit tests for HttpTraceLoggingHandler

---
+++ http-native/src/test/java/org/ballerinalang/net/transport/contractimpl/listener/HttpTraceLoggingHandlerTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.ballerinalang.net.transport.contractimpl.listener;
+
+import io.netty.handler.logging.LogLevel;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * A unit test class for Transport module HttpTraceLoggingHandler class functions.
+ */
+public class HttpTraceLoggingHandlerTest {
+
+    @Test
+    public void testHttpTraceLoggingHandler() {
+        HttpTraceLoggingHandler httpTraceLoggingHandler = new HttpTraceLoggingHandler(LogLevel.INFO);
+        Assert.assertNotNull(httpTraceLoggingHandler);
+
+        httpTraceLoggingHandler = new HttpTraceLoggingHandler(String.class);
+        Assert.assertNotNull(httpTraceLoggingHandler);
+
+        httpTraceLoggingHandler = new HttpTraceLoggingHandler(String.class, LogLevel.INFO);
+        Assert.assertNotNull(httpTraceLoggingHandler);
+
+        httpTraceLoggingHandler = new HttpTraceLoggingHandler("name");
+        Assert.assertNotNull(httpTraceLoggingHandler);
+
+        httpTraceLoggingHandler = new HttpTraceLoggingHandler("name", LogLevel.INFO);
+        Assert.assertNotNull(httpTraceLoggingHandler);
+    }
+
+    @Test
+    public void testSetCorrelatedSourceId() {
+        HttpTraceLoggingHandler httpTraceLoggingHandler = new HttpTraceLoggingHandler(LogLevel.INFO);
+        httpTraceLoggingHandler.setCorrelatedSourceId("testId");
+    }
+
+}
