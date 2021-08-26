389bc4c634dba77c29d3445c39ee28747249d2ae
dilanSachi <dilansachinthanos@gmail.com>
2021-08-26 17:55:36 +0530

    Add license header to tests

--- native/src/test/java/io/ballerina/stdlib/http/transport/http2/Http2WithHttp2ResetContent.java
+++ native/src/test/java/io/ballerina/stdlib/http/transport/http2/Http2WithHttp2ResetContent.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
 package io.ballerina.stdlib.http.transport.http2;
 
 import io.ballerina.stdlib.http.transport.contentaware.listeners.EchoMessageListener;
@@ -59,8 +78,8 @@ public class Http2WithHttp2ResetContent {
         senderConfiguration.setHttpVersion(Constants.HTTP_2_0);
         senderConfiguration.setForceHttp2(true);       // Force to use HTTP/2 without an upgrade
         connectionManager = new ConnectionManager(senderConfiguration.getPoolConfiguration());
-        httpClientConnector = connectorFactory.createHttpClientConnector(
-                HttpConnectorUtil.getTransportProperties(transportsConfiguration), senderConfiguration, connectionManager);
+        httpClientConnector = connectorFactory.createHttpClientConnector(HttpConnectorUtil.getTransportProperties(
+                transportsConfiguration), senderConfiguration, connectionManager);
     }
 
     @Test(description = "Sends a request with reset content such that the stream will be reset")
