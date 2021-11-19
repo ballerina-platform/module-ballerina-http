2c2a3d9574068510a2ae7f0fcb4f580bcb7e4724
TharmiganK <ktharmi176@gmail.com>
2021-11-19 16:50:13 +0530

    Fix interceptor compiler plugin bug

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/CompilerPluginTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/CompilerPluginTest.java
@@ -410,7 +410,7 @@ public class CompilerPluginTest {
         Package currentPackage = loadPackage("sample_package_19");
         PackageCompilation compilation = currentPackage.getCompilation();
         DiagnosticResult diagnosticResult = compilation.diagnosticResult();
-        Assert.assertEquals(diagnosticResult.errorCount(), 8);
+        Assert.assertEquals(diagnosticResult.errorCount(), 9);
         assertError(diagnosticResult, 0, "invalid multiple interceptor type reference: " +
                 "'http:RequestErrorInterceptor'", HTTP_123);
         assertError(diagnosticResult, 1, "invalid interceptor resource path: expected default resource" +
@@ -426,6 +426,8 @@ public class CompilerPluginTest {
         assertError(diagnosticResult, 6, "invalid multiple interceptor resource functions", HTTP_124);
         assertError(diagnosticResult, 7, "invalid annotation 'http:ResourceConfig': annotations" +
                 " are not supported for interceptor resource functions", HTTP_125);
+        assertError(diagnosticResult, 8, "invalid interceptor resource path: expected default resource" +
+                " path: '[string... path]', but found '[string path]'", HTTP_127);
     }
 
 }
