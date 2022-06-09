6500e4f4049c8e33183d4e41303a6d31ffd99d08
Chamil Elladeniya <chamilmadawa8@gmail.com>
2022-06-09 22:22:36 +0530

    Change immediate stop close the socket pipeline rather closing eventloops

--- native/src/main/java/io/ballerina/stdlib/http/api/HttpUtil.java
+++ native/src/main/java/io/ballerina/stdlib/http/api/HttpUtil.java
@@ -63,7 +63,6 @@ import io.ballerina.stdlib.http.transport.contract.exceptions.EndpointTimeOutExc
 import io.ballerina.stdlib.http.transport.contract.exceptions.PromiseRejectedException;
 import io.ballerina.stdlib.http.transport.contract.exceptions.ServerConnectorException;
 import io.ballerina.stdlib.http.transport.contract.exceptions.SslException;
-import io.ballerina.stdlib.http.transport.contractimpl.DefaultHttpWsConnectorFactory;
 import io.ballerina.stdlib.http.transport.contractimpl.sender.channel.pool.ConnectionManager;
 import io.ballerina.stdlib.http.transport.contractimpl.sender.channel.pool.PoolConfiguration;
 import io.ballerina.stdlib.http.transport.message.Http2PushPromise;
@@ -1143,7 +1142,7 @@ public class HttpUtil {
     }
 
     public static HttpWsConnectorFactory createHttpWsConnectionFactory() {
-        return new DefaultHttpWsConnectorFactory();
+        return DefaultHttpWsConnectorFactoryHolder.getHttpConnectorFactory();
     }
 
     public static void checkAndObserveHttpRequest(Environment environment, HttpCarbonMessage message) {
