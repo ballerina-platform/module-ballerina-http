b45976f0d078209bd1f935fa538b0b497282d2ec
ayeshLK <ayesh9303@gmail.com>
2022-06-22 11:36:39 +0530

    Revert unwanted changes

--- native/src/main/java/io/ballerina/stdlib/http/api/HttpResponseInterceptorUnitCallback.java
+++ native/src/main/java/io/ballerina/stdlib/http/api/HttpResponseInterceptorUnitCallback.java
@@ -146,7 +146,7 @@ public class HttpResponseInterceptorUnitCallback implements Callback {
     }
 
     private void returnResponse(Object result) {
-        Object[] paramFeed = new Object[10];
+        Object[] paramFeed = new Object[8];
         paramFeed[0] = result;
         paramFeed[1] = true;
         paramFeed[2] = null;
@@ -155,8 +155,6 @@ public class HttpResponseInterceptorUnitCallback implements Callback {
         paramFeed[5] = true;
         paramFeed[6] = null;
         paramFeed[7] = true;
-        paramFeed[8] = null;
-        paramFeed[9] = true;
 
         invokeBalMethod(paramFeed, "returnResponse");
     }
