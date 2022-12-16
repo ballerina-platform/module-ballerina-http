5f3459302e34670bb1b5e2c8ec93a27e19b92a3a
TharmiganK <ktharmi176@gmail.com>
2022-12-16 20:00:02 +0530

    Remove testng usage from testutils

--- test-utils/src/main/java/io/ballerina/stdlib/http/testutils/ExternRetryMultipartTestutil.java
+++ test-utils/src/main/java/io/ballerina/stdlib/http/testutils/ExternRetryMultipartTestutil.java
@@ -25,7 +25,6 @@ import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.Assert;
 
 import java.io.IOException;
 import java.util.HashMap;
