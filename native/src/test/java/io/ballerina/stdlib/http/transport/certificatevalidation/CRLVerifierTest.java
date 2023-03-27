d83e4cd179ea92211c1fecba0b16844aede04760
bhashinee <bhashineen@gmail.com>
2023-03-27 10:31:06 +0530

    Remove unused imports

--- native/src/test/java/io/ballerina/stdlib/http/transport/certificatevalidation/CRLVerifierTest.java
+++ native/src/test/java/io/ballerina/stdlib/http/transport/certificatevalidation/CRLVerifierTest.java
@@ -29,7 +29,6 @@ import org.bouncycastle.cert.X509v2CRLBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CRLConverter;
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils;
 import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
-import org.bouncycastle.jce.PrincipalUtil;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.testng.annotations.Test;
 
