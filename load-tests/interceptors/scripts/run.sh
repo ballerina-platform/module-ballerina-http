bf06af36e73df5d0c4711144ce01ff6adca509d1
TharmiganK <ktharmi176@gmail.com>
2023-04-17 13:49:45 +0530

    Update license header

--- load-tests/interceptors/scripts/run.sh
+++ load-tests/interceptors/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# Copyright 2021 WSO2 Inc. (http://wso2.org)
+# Copyright 2023 WSO2 Inc. (http://wso2.org)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
