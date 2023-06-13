c604998246ca228e43bc4fc7a3e10ad40b13bf22
dilanSachi <dilansachinthanos@gmail.com>
2023-06-13 13:18:17 +0530

    Use constant values

--- native/src/main/java/io/ballerina/stdlib/http/api/HttpDispatcher.java
+++ native/src/main/java/io/ballerina/stdlib/http/api/HttpDispatcher.java
@@ -55,9 +55,11 @@ import java.util.concurrent.CountDownLatch;
 import static io.ballerina.stdlib.http.api.HttpConstants.AUTHORIZATION_HEADER;
 import static io.ballerina.stdlib.http.api.HttpConstants.BEARER_AUTHORIZATION_HEADER;
 import static io.ballerina.stdlib.http.api.HttpConstants.DEFAULT_HOST;
+import static io.ballerina.stdlib.http.api.HttpConstants.EMPTY;
 import static io.ballerina.stdlib.http.api.HttpConstants.JWT_DECODER_CLASS_NAME;
 import static io.ballerina.stdlib.http.api.HttpConstants.JWT_DECODE_METHOD_NAME;
 import static io.ballerina.stdlib.http.api.HttpConstants.JWT_INFORMATION;
+import static io.ballerina.stdlib.http.api.HttpConstants.QUERY_STRING_SEPARATOR;
 import static io.ballerina.stdlib.http.api.HttpConstants.REQUEST_CTX_MEMBERS;
 import static io.ballerina.stdlib.http.api.HttpConstants.WHITESPACE;
 import static io.ballerina.stdlib.http.api.HttpErrorType.SERVICE_NOT_FOUND_ERROR;
@@ -179,9 +181,9 @@ public class HttpDispatcher {
 
     private static String[] extractRawPathAndQuery(String uriWithoutMatrixParams) {
         String[] rawPathAndQuery = new String[2];
-        String[] splittedUri = uriWithoutMatrixParams.split("\\?");
+        String[] splittedUri = uriWithoutMatrixParams.split(QUERY_STRING_SEPARATOR);
         rawPathAndQuery[0] = splittedUri[0];
-        rawPathAndQuery[1] = splittedUri.length > 1 ? splittedUri[1] : "";
+        rawPathAndQuery[1] = splittedUri.length > 1 ? splittedUri[1] : EMPTY;
         return rawPathAndQuery;
     }
 
