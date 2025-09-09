b3d309d4923b674270cb01527c303af8cf0483ea
Nureka Rodrigo <nurekadulshanmc@gmail.com>
2025-09-09 14:39:42 +0530

    Refactor StaticCodeAnalyzerTest to improve readability and modularity

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/staticcodeanalyzer/StaticCodeAnalyzerTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/staticcodeanalyzer/StaticCodeAnalyzerTest.java
@@ -71,90 +71,112 @@ public class StaticCodeAnalyzerTest {
     public void testStaticCodeRulesWithAPI() throws IOException {
         ByteArrayOutputStream console = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(console, true, UTF_8);
+
         for (HttpRule rule : HttpRule.values()) {
-            String targetPackageName = "rule" + rule.getId();
-            Path targetPackagePath = RESOURCE_PACKAGES_DIRECTORY.resolve(targetPackageName);
-            Project project = BuildProject.load(getEnvironmentBuilder(), targetPackagePath);
-            TestOptions options = TestOptions.builder(project).setOutputStream(printStream).build();
-            TestRunner testRunner = new TestRunner(options);
-            testRunner.performScan();
-
-            // validate the rules
-            List<Rule> rules = testRunner.getRules();
-            Assertions.assertRule(
-                    rules,
-                    "ballerina/http:1",
-                    AVOID_DEFAULT_RESOURCE_ACCESSOR.getDescription(),
-                    VULNERABILITY);
-            Assertions.assertRule(
-                    rules,
-                    "ballerina/http:2",
-                    AVOID_PERMISSIVE_CORS.getDescription(),
-                    VULNERABILITY);
-            Assertions.assertRule(
-                    rules,
-                    "ballerina/http:3",
-                    AVOID_TRAVERSING_ATTACKS.getDescription(),
-                    VULNERABILITY);
-
-            // validate the issues
-            List<Issue> issues = testRunner.getIssues();
-            int index = 0;
-
-            if (rule == AVOID_DEFAULT_RESOURCE_ACCESSOR) {
-                Assert.assertEquals(issues.size(), 8);
-                Assertions.assertIssue(issues, index++, "ballerina/http:1", "service.bal",
-                        19, 19, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:1", "service.bal",
-                        23, 23, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:1", "service_class.bal",
-                        21, 21, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:1", "service_class.bal",
-                        25, 25, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:1", "service_object.bal",
-                        20, 20, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:1", "service_object.bal",
-                        21, 21, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:1", "service_object.bal",
-                        26, 26, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index, "ballerina/http:1", "service_object.bal",
-                        27, 27, Source.BUILT_IN);
-            } else if (rule == AVOID_PERMISSIVE_CORS) {
-                Assert.assertEquals(issues.size(), 7);
-                Assertions.assertIssue(issues, index++, "ballerina/http:2", "service.bal",
-                        20, 20, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:2", "service.bal",
-                        27, 27, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:2", "service_class.bal",
-                        23, 23, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:2", "service_object.bal",
-                        20, 20, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:2", "service_object.bal",
-                        28, 28, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:2", "service_object.bal",
-                        36, 36, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index, "ballerina/http:2", "service_object.bal",
-                        44, 44, Source.BUILT_IN);
-            } else if (rule == AVOID_TRAVERSING_ATTACKS) {
-                Assert.assertEquals(issues.size(), 5);
-                Assertions.assertIssue(issues, index++, "ballerina/http:3", "function.bal",
-                        30, 30, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:3", "service.bal",
-                        21, 21, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:3", "service.bal",
-                        27, 27, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index++, "ballerina/http:3", "service_class.bal",
-                        21, 21, Source.BUILT_IN);
-                Assertions.assertIssue(issues, index, "ballerina/http:3", "service_class.bal",
-                        27, 27, Source.BUILT_IN);
-            }
-
-            // validate the output
-            String output = console.toString(UTF_8);
-            String jsonOutput = extractJson(output);
-            String expectedOutput = Files.readString(EXPECTED_OUTPUT_DIRECTORY.resolve(targetPackageName + ".json"));
-            assertJsonEqual(jsonOutput, expectedOutput);
+            testIndividualRule(rule, console, printStream);
         }
+    }
+
+    private void testIndividualRule(HttpRule rule, ByteArrayOutputStream console, PrintStream printStream)
+            throws IOException {
+        String targetPackageName = "rule" + rule.getId();
+        Path targetPackagePath = RESOURCE_PACKAGES_DIRECTORY.resolve(targetPackageName);
+
+        TestRunner testRunner = setupTestRunner(targetPackagePath, printStream);
+        testRunner.performScan();
+
+        validateRules(testRunner.getRules());
+        validateIssues(rule, testRunner.getIssues());
+        validateOutput(console, targetPackageName);
+
+        console.reset();
+    }
+
+    private TestRunner setupTestRunner(Path targetPackagePath, PrintStream printStream) {
+        Project project = BuildProject.load(getEnvironmentBuilder(), targetPackagePath);
+        TestOptions options = TestOptions.builder(project).setOutputStream(printStream).build();
+        return new TestRunner(options);
+    }
+
+    private void validateRules(List<Rule> rules) {
+        Assertions.assertRule(
+                rules,
+                "ballerina/http:1",
+                AVOID_DEFAULT_RESOURCE_ACCESSOR.getDescription(),
+                VULNERABILITY);
+        Assertions.assertRule(
+                rules,
+                "ballerina/http:2",
+                AVOID_PERMISSIVE_CORS.getDescription(),
+                VULNERABILITY);
+        Assertions.assertRule(
+                rules,
+                "ballerina/http:3",
+                AVOID_TRAVERSING_ATTACKS.getDescription(),
+                VULNERABILITY);
+    }
+
+    private void validateIssues(HttpRule rule, List<Issue> issues) {
+        switch (rule) {
+            case AVOID_DEFAULT_RESOURCE_ACCESSOR:
+                Assert.assertEquals(issues.size(), 8);
+                Assertions.assertIssue(issues, 0, "ballerina/http:1", "service.bal",
+                        19, 19, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 1, "ballerina/http:1", "service.bal",
+                        23, 23, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 2, "ballerina/http:1", "service_class.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 3, "ballerina/http:1", "service_class.bal",
+                        25, 25, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 4, "ballerina/http:1", "service_object.bal",
+                        20, 20, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 5, "ballerina/http:1", "service_object.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 6, "ballerina/http:1", "service_object.bal",
+                        26, 26, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 7, "ballerina/http:1", "service_object.bal",
+                        27, 27, Source.BUILT_IN);
+                break;
+            case AVOID_PERMISSIVE_CORS:
+                Assert.assertEquals(issues.size(), 7);
+                Assertions.assertIssue(issues, 0, "ballerina/http:2", "service.bal",
+                        20, 20, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 1, "ballerina/http:2", "service.bal",
+                        27, 27, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 2, "ballerina/http:2", "service_class.bal",
+                        23, 23, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 3, "ballerina/http:2", "service_object.bal",
+                        20, 20, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 4, "ballerina/http:2", "service_object.bal",
+                        28, 28, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 5, "ballerina/http:2", "service_object.bal",
+                        36, 36, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 6, "ballerina/http:2", "service_object.bal",
+                        44, 44, Source.BUILT_IN);
+                break;
+            case AVOID_TRAVERSING_ATTACKS:
+                Assert.assertEquals(issues.size(), 5);
+                Assertions.assertIssue(issues, 0, "ballerina/http:3", "function.bal",
+                        30, 30, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 1, "ballerina/http:3", "service.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 2, "ballerina/http:3", "service.bal",
+                        27, 27, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 3, "ballerina/http:3", "service_class.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 4, "ballerina/http:3", "service_class.bal",
+                        27, 27, Source.BUILT_IN);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void validateOutput(ByteArrayOutputStream console, String targetPackageName) throws IOException {
+        String output = console.toString(UTF_8);
+        String jsonOutput = extractJson(output);
+        String expectedOutput = Files.readString(EXPECTED_OUTPUT_DIRECTORY.resolve(targetPackageName + ".json"));
+        assertJsonEqual(jsonOutput, expectedOutput);
     }
 
     private static ProjectEnvironmentBuilder getEnvironmentBuilder() {
