8a10987dba1ec02b89bcfe9ab9b2f33637246df4
Nureka Rodrigo <127966714+nureka-rodrigo@users.noreply.github.com>
2025-09-16 16:30:14 +0530

    Implement static code analysis rule to analyze vulnerable server-side requests (#2470)
    * Update static code analyzer test to use scan-command API and bump scan tool version
    
    * Add a scan rule to analyze vulnerable server-side requests
    
    * Make RuleFactory class final
    
    * Add getDescription method to HttpRule and update tests to use dynamic descriptions
    
    * [Automated] Update the native jar versions
    
    * [Automated] Update the native jar versions
    
    * [Automated] Update the native jar versions
    
    * Remove unused import for RuleKind in StaticCodeAnalyzerTest
    
    * Refactor StaticCodeAnalyzerTest to improve readability and modularity
    
    * Add Jackson Databind dependency and refactor JSON normalization in StaticCodeAnalyzerTest
    
    * [Automated] Update the native jar versions
    
    * Add assertion failure in default branch of validateIssues switch and replace generic Exception catch with specific exceptions
    
    * Update changelog.md

--- changelog.md
+++ changelog.md
@@ -1,4 +1,3 @@
-
 # Change Log
 This file contains all the notable changes done to the Ballerina HTTP package through the releases.
 
@@ -18,6 +17,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 ### Added
 
 - [Add support for dispatching URLs with special characters to the associated resource signature with special characters](https://github.com/ballerina-platform/ballerina-library/issues/4154)
+- [Implement static analysis rule to analyze server-side requests that are vulnerable to traversing attacks](https://github.com/ballerina-platform/ballerina-library/issues/8175)
 
 ## [2.14.2] - 2025-06-23
 
--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/staticcodeanalyzer/StaticCodeAnalyzerTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/staticcodeanalyzer/StaticCodeAnalyzerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.org)
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -18,109 +18,201 @@
 
 package io.ballerina.stdlib.http.compiler.staticcodeanalyzer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.ballerina.projects.Project;
+import io.ballerina.projects.ProjectEnvironmentBuilder;
+import io.ballerina.projects.directory.BuildProject;
+import io.ballerina.projects.environment.Environment;
+import io.ballerina.projects.environment.EnvironmentBuilder;
+import io.ballerina.scan.Issue;
+import io.ballerina.scan.Rule;
+import io.ballerina.scan.Source;
+import io.ballerina.scan.test.Assertions;
+import io.ballerina.scan.test.TestOptions;
+import io.ballerina.scan.test.TestRunner;
 import org.testng.Assert;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
-import org.testng.internal.ExitCode;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
-import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
 
-/**
- * This class includes tests for Ballerina Http static code analyzer.
- */
-public class StaticCodeAnalyzerTest {
+import static io.ballerina.scan.RuleKind.VULNERABILITY;
+import static io.ballerina.stdlib.http.compiler.staticcodeanalyzer.HttpRule.AVOID_DEFAULT_RESOURCE_ACCESSOR;
+import static io.ballerina.stdlib.http.compiler.staticcodeanalyzer.HttpRule.AVOID_PERMISSIVE_CORS;
+import static io.ballerina.stdlib.http.compiler.staticcodeanalyzer.HttpRule.AVOID_TRAVERSING_ATTACKS;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
+public class StaticCodeAnalyzerTest {
     private static final Path RESOURCE_PACKAGES_DIRECTORY = Paths
             .get("src", "test", "resources", "static_code_analyzer", "ballerina_packages").toAbsolutePath();
-    private static final Path EXPECTED_JSON_OUTPUT_DIRECTORY = Paths.
-            get("src", "test", "resources", "static_code_analyzer", "expected_output").toAbsolutePath();
-    private static final Path BALLERINA_PATH = getBalCommandPath();
+    private static final Path EXPECTED_OUTPUT_DIRECTORY = Paths
+            .get("src", "test", "resources", "static_code_analyzer", "expected_output").toAbsolutePath();
     private static final Path JSON_RULES_FILE_PATH = Paths
             .get("../", "compiler-plugin", "src", "main", "resources", "rules.json").toAbsolutePath();
-    private static final String SCAN_COMMAND = "scan";
-
-    private static Path getBalCommandPath() {
-        String balCommand = isWindows() ? "bal.bat" : "bal";
-        return Paths.get("../", "target", "ballerina-runtime", "bin", balCommand).toAbsolutePath();
-    }
-
-    @BeforeSuite
-    public void pullScanTool() throws IOException, InterruptedException {
-        ProcessBuilder processBuilder = new ProcessBuilder(BALLERINA_PATH.toString(), "tool", "pull", SCAN_COMMAND);
-        ProcessOutputGobbler output = getOutput(processBuilder.start());
-        if (Pattern.compile("tool 'scan:.+\\..+\\..+' successfully set as the active version\\.")
-                .matcher(output.getOutput()).find() || Pattern.compile("tool 'scan:.+\\..+\\..+' is already active\\.")
-                .matcher(output.getOutput()).find()) {
-            return;
-        }
-        Assert.assertFalse(ExitCode.hasFailure(output.getExitCode()));
-    }
+    private static final Path DISTRIBUTION_PATH = Paths.get("../", "target", "ballerina-runtime");
+    private static final String MODULE_BALLERINA_HTTP = "module-ballerina-http";
 
     @Test
     public void validateRulesJson() throws IOException {
         String expectedRules = "[" + Arrays.stream(HttpRule.values())
                 .map(HttpRule::toString).collect(Collectors.joining(",")) + "]";
         String actualRules = Files.readString(JSON_RULES_FILE_PATH);
-        assertJsonEqual(normalizeJson(actualRules), normalizeJson(expectedRules));
+        assertJsonEqual(actualRules, expectedRules);
     }
 
     @Test
-    public void testStaticCodeRules() throws IOException, InterruptedException {
+    public void testStaticCodeRulesWithAPI() throws IOException {
+        ByteArrayOutputStream console = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(console, true, UTF_8);
+
         for (HttpRule rule : HttpRule.values()) {
-            String targetPackageName = "rule" + rule.getId();
-            String actualJsonReport = StaticCodeAnalyzerTest.executeScanProcess(targetPackageName);
-            String expectedJsonReport = Files
-                    .readString(EXPECTED_JSON_OUTPUT_DIRECTORY.resolve(targetPackageName + ".json"));
-            assertJsonEqual(actualJsonReport, expectedJsonReport);
+            testIndividualRule(rule, console, printStream);
         }
     }
 
-    public static String executeScanProcess(String targetPackage) throws IOException, InterruptedException {
-        ProcessBuilder processBuilder = new ProcessBuilder(BALLERINA_PATH.toString(), SCAN_COMMAND);
-        processBuilder.directory(RESOURCE_PACKAGES_DIRECTORY.resolve(targetPackage).toFile());
-        ProcessOutputGobbler output = getOutput(processBuilder.start());
-        Assert.assertFalse(ExitCode.hasFailure(output.getExitCode()));
-        return Files.readString(RESOURCE_PACKAGES_DIRECTORY.resolve(targetPackage)
-                .resolve("target").resolve("report").resolve("scan_results.json"));
+    private void testIndividualRule(HttpRule rule, ByteArrayOutputStream console, PrintStream printStream)
+            throws IOException {
+        String targetPackageName = "rule" + rule.getId();
+        Path targetPackagePath = RESOURCE_PACKAGES_DIRECTORY.resolve(targetPackageName);
+
+        TestRunner testRunner = setupTestRunner(targetPackagePath, printStream);
+        testRunner.performScan();
+
+        validateRules(testRunner.getRules());
+        validateIssues(rule, testRunner.getIssues());
+        validateOutput(console, targetPackageName);
+
+        console.reset();
     }
 
-    private static ProcessOutputGobbler getOutput(Process process) throws InterruptedException {
-        ProcessOutputGobbler outputGobbler = new ProcessOutputGobbler(process.getInputStream());
-        ProcessOutputGobbler errorGobbler = new ProcessOutputGobbler(process.getErrorStream());
-        Thread outputThread = new Thread(outputGobbler);
-        Thread errorThread = new Thread(errorGobbler);
-        outputThread.start();
-        errorThread.start();
-        int exitCode = process.waitFor();
-        outputGobbler.setExitCode(exitCode);
-        errorGobbler.setExitCode(exitCode);
-        outputThread.join();
-        errorThread.join();
-        return outputGobbler;
+    private TestRunner setupTestRunner(Path targetPackagePath, PrintStream printStream) {
+        Project project = BuildProject.load(getEnvironmentBuilder(), targetPackagePath);
+        TestOptions options = TestOptions.builder(project).setOutputStream(printStream).build();
+        return new TestRunner(options);
+    }
+
+    private void validateRules(List<Rule> rules) {
+        Assertions.assertRule(
+                rules,
+                "ballerina/http:1",
+                AVOID_DEFAULT_RESOURCE_ACCESSOR.getDescription(),
+                VULNERABILITY);
+        Assertions.assertRule(
+                rules,
+                "ballerina/http:2",
+                AVOID_PERMISSIVE_CORS.getDescription(),
+                VULNERABILITY);
+        Assertions.assertRule(
+                rules,
+                "ballerina/http:3",
+                AVOID_TRAVERSING_ATTACKS.getDescription(),
+                VULNERABILITY);
+    }
+
+    private void validateIssues(HttpRule rule, List<Issue> issues) {
+        switch (rule) {
+            case AVOID_DEFAULT_RESOURCE_ACCESSOR:
+                Assert.assertEquals(issues.size(), 8);
+                Assertions.assertIssue(issues, 0, "ballerina/http:1", "service.bal",
+                        19, 19, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 1, "ballerina/http:1", "service.bal",
+                        23, 23, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 2, "ballerina/http:1", "service_class.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 3, "ballerina/http:1", "service_class.bal",
+                        25, 25, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 4, "ballerina/http:1", "service_object.bal",
+                        20, 20, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 5, "ballerina/http:1", "service_object.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 6, "ballerina/http:1", "service_object.bal",
+                        26, 26, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 7, "ballerina/http:1", "service_object.bal",
+                        27, 27, Source.BUILT_IN);
+                break;
+            case AVOID_PERMISSIVE_CORS:
+                Assert.assertEquals(issues.size(), 7);
+                Assertions.assertIssue(issues, 0, "ballerina/http:2", "service.bal",
+                        20, 20, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 1, "ballerina/http:2", "service.bal",
+                        27, 27, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 2, "ballerina/http:2", "service_class.bal",
+                        23, 23, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 3, "ballerina/http:2", "service_object.bal",
+                        20, 20, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 4, "ballerina/http:2", "service_object.bal",
+                        28, 28, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 5, "ballerina/http:2", "service_object.bal",
+                        36, 36, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 6, "ballerina/http:2", "service_object.bal",
+                        44, 44, Source.BUILT_IN);
+                break;
+            case AVOID_TRAVERSING_ATTACKS:
+                Assert.assertEquals(issues.size(), 5);
+                Assertions.assertIssue(issues, 0, "ballerina/http:3", "function.bal",
+                        30, 30, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 1, "ballerina/http:3", "service.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 2, "ballerina/http:3", "service.bal",
+                        27, 27, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 3, "ballerina/http:3", "service_class.bal",
+                        21, 21, Source.BUILT_IN);
+                Assertions.assertIssue(issues, 4, "ballerina/http:3", "service_class.bal",
+                        27, 27, Source.BUILT_IN);
+                break;
+            default:
+                Assert.fail("Unhandled rule in validateIssues: " + rule);
+                break;
+        }
+    }
+
+    private void validateOutput(ByteArrayOutputStream console, String targetPackageName) throws IOException {
+        String output = console.toString(UTF_8);
+        String jsonOutput = extractJson(output);
+        String expectedOutput = Files.readString(EXPECTED_OUTPUT_DIRECTORY.resolve(targetPackageName + ".json"));
+        assertJsonEqual(jsonOutput, expectedOutput);
+    }
+
+    private static ProjectEnvironmentBuilder getEnvironmentBuilder() {
+        Environment environment = EnvironmentBuilder.getBuilder().setBallerinaHome(DISTRIBUTION_PATH).build();
+        return ProjectEnvironmentBuilder.getBuilder(environment);
+    }
+
+    private String extractJson(String consoleOutput) {
+        int startIndex = consoleOutput.indexOf("[");
+        int endIndex = consoleOutput.lastIndexOf("]");
+        if (startIndex == -1 || endIndex == -1) {
+            return "";
+        }
+        return consoleOutput.substring(startIndex, endIndex + 1);
     }
 
     private void assertJsonEqual(String actual, String expected) {
-        Assert.assertEquals(normalizeJson(actual), normalizeJson(expected));
+        Assert.assertEquals(normalizeString(actual), normalizeString(expected));
     }
 
-    private static String normalizeJson(String json) {
-        String normalizedJson = json.replaceAll("\\s*\"\\s*", "\"")
-                .replaceAll("\\s*:\\s*", ":")
-                .replaceAll("\\s*,\\s*", ",")
-                .replaceAll("\\s*\\{\\s*", "{")
-                .replaceAll("\\s*}\\s*", "}")
-                .replaceAll("\\s*\\[\\s*", "[")
-                .replaceAll("\\s*]\\s*", "]")
-                .replaceAll("\n", "")
-                .replaceAll(":\".*module-ballerina-http", ":\"module-ballerina-http");
-        return isWindows() ? normalizedJson.replaceAll("/", "\\\\\\\\") : normalizedJson;
+    private static String normalizeString(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper().configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+            JsonNode node = mapper.readTree(json);
+            String normalizedJson = mapper.writeValueAsString(node)
+                    .replaceAll(":\".*" + MODULE_BALLERINA_HTTP, ":\"" + MODULE_BALLERINA_HTTP);
+            return isWindows() ? normalizedJson.replace("/", "\\\\") : normalizedJson;
+        } catch (JsonProcessingException | PatternSyntaxException ignore) {
+            return json;
+        }
     }
 
     private static boolean isWindows() {
