f4b3f8ff984742b1704b25ac15512b8e65a0a0e9
Suthankan Balenthiran <Suthankanbala2019@gmail.com>
2026-09-14 16:41:05 +0530

    Fix ambiguity diagnostics for HTTP service forms

--- compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/CompilerPluginTest.java
+++ compiler-plugin-tests/src/test/java/io/ballerina/stdlib/http/compiler/CompilerPluginTest.java
@@ -960,4 +960,18 @@ public class CompilerPluginTest {
         DiagnosticResult diagnosticResult = compilation.diagnosticResult();
         Assert.assertEquals(diagnosticResult.errorCount(), 0);
     }
+
+    @Test
+    public void testAmbiguousPayloadParamInServiceClassAndObject() {
+        Package currentPackage = loadPackage("sample_package_53");
+        PackageCompilation compilation = currentPackage.getCompilation();
+        DiagnosticResult diagnosticResult = compilation.diagnosticResult();
+        Assert.assertEquals(diagnosticResult.errorCount(), 3);
+        assertTrue(diagnosticResult, 0, "ambiguous types for parameter 'p1' and 'p2'. Use " +
+                "annotations to avoid ambiguity", HTTP_151);
+        assertTrue(diagnosticResult, 1, "ambiguous types for parameter 'p1' and 'p2'. Use " +
+                "annotations to avoid ambiguity", HTTP_151);
+        assertTrue(diagnosticResult, 2, "ambiguous types for parameter 'p1' and 'p2'. Use " +
+                "annotations to avoid ambiguity", HTTP_151);
+    }
 }
